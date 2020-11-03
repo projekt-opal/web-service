efc4b4fb98d681ff6fb93b94613eadc9debbc526
pskLana <48616148+pskLana@users.noreply.github.com>
2020-11-03 14:44:58 +0100

    Added retrieval of the catalogs info

--- src/main/java/org/dice_research/opal/webservice/services/ElasticSearchProvider.java
+++ src/main/java/org/dice_research/opal/webservice/services/ElasticSearchProvider.java
@@ -30,7 +30,6 @@ import org.elasticsearch.search.sort.SortBuilders;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -171,6 +170,7 @@ public class ElasticSearchProvider {
 		try {
 			ret.add(getFilterListOfThemes(searchDTO, uri));
 			ret.add(getFilterListOfPublishers(searchDTO, uri));
+			ret.add(getFilterListOfCatalogs(searchDTO, uri));
 			ret.add(getFilterListOfLicenses(searchDTO, uri));
 //            ret.add(getIssueDateFilter());
 		} catch (IOException e) {
@@ -379,6 +379,35 @@ public class ElasticSearchProvider {
 		return FilterDTO.builder().filterGroupTitle("Publisher").hasExternalLink(true).hasStaticValues(false)
 				.values(values).searchField(searchField).build();
 	}
+	
+	private FilterDTO getFilterListOfCatalogs(SearchDTO searchDTO, String uri) throws IOException {
+		String searchField = "catalog";
+//		HashMap<String, List<ValueDTO>> catalogeMeasures = new HashMap<String, List<ValueDTO>>();
+		List<String> catalogeURIs = new ArrayList<String>( Arrays.asList(
+				"https://mcloud.de/", 
+				"https://www.govdata.de/", 
+				"https://www.europeandataportal.eu/",
+				"https://service.mdm-portal.de/"));
+//		for (String catalog : catalogeURIs) {
+//			List<ValueDTO> amountOfValues = calculateTopCatalogeValues(searchDTO, searchField, null, catalog);
+//			catalogeMeasures.put(catalog, amountOfValues);
+//        }
+		
+		List<ValueDTO> values = new ArrayList<>();
+		for (String i : catalogeURIs) {
+			ValueDTO valueDTO = new ValueDTO();
+			valueDTO.setValue(i);
+			valueDTO.setCount(new CounterDTO());
+			values.add(valueDTO);
+		}
+		
+
+		updateRelativeCount(searchDTO, uri, searchField, values);
+		updateAbsoluteCount(searchDTO, uri, searchField, values);
+
+		return FilterDTO.builder().filterGroupTitle("Catalog").hasExternalLink(true).hasStaticValues(true).values(values)
+				.searchField(searchField).build();
+	}
 
 	private void updateRelativeCount(SearchDTO searchDTO, String uri, String searchField, List<ValueDTO> values) {
 
@@ -401,6 +430,44 @@ public class ElasticSearchProvider {
 		});
 
 		return values;
+	}
+	
+	private List<ValueDTO> calculateTopCatalogeValues(SearchDTO searchDTO, String searchField, String containsText, String catalog)
+			throws IOException {
+		List<ValueDTO> values = new ArrayList<>();
+		SearchRequest searchRequest = new SearchRequest();
+		searchRequest.indices(es_index);
+		
+		SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+		searchSourceBuilder.size(1);
+		
+		searchSourceBuilder.query(
+                        QueryBuilders.termQuery(
+                        		searchField, catalog)
+                        );
+		
+		searchRequest.source(searchSourceBuilder);
+
+		try {
+            SearchResponse searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+
+            if (searchResponse.getHits().getTotalHits().value >= 1) {
+				SearchHit[] hits = searchResponse.getHits().getHits();
+				SearchHit hit = hits[0];
+				String sourceAsString = hit.getSourceAsString();			
+				ValueDTO valueDTO = new ValueDTO();
+				valueDTO.setValue(sourceAsString);
+				valueDTO.setCount(new CounterDTO());
+				values.add(valueDTO);
+			}
+            
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        
+        return values;
 	}
 
 	private List<ValueDTO> calculateTopValues(SearchDTO searchDTO, String searchField, String containsText)
